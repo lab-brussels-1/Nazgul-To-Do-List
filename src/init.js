3e9bf1d4cb344c0c498943db1022a0833ae33943
Samir M <67711948+samirm00@users.noreply.github.com>
2022-08-27 21:10:08 +0200

    Update init.js

--- src/init.js
+++ src/init.js
@@ -2,7 +2,7 @@ const todos = async() =>
 {
    try
    {
-        const res = await  fetch("https://my-json-server.typicode.com/samirm00/todolist/db");
+        const res = await  fetch("https://my-json-server.typicode.com/samirm00/todolist/todos");
         
         if (res.status === 200)
         {
