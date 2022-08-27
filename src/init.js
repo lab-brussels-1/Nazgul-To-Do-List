9331a2abdb0344d591f2ab17c7ddc7f00d276c39
samirm00 <sammoham.be@gmail.com>
2022-08-27 16:52:20 +0200

    test json-server

--- src/init.js
+++ src/init.js
@@ -2,7 +2,7 @@ const todos = async() =>
 {
    try
    {
-        const res = await  fetch("/todos");
+        const res = await  fetch("https://my-json-server.typicode.com/samirm00/todolist");
         
         if (res.status === 200)
         {
